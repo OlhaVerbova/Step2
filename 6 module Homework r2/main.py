180783a3537c328f57c3182b2fd24efddbd2bac8
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2023-07-25 18:08:43 +0300

    Update main.py

--- 6 module Homework r2/main.py
+++ 6 module Homework r2/main.py
@@ -4,9 +4,9 @@ from parsing_the_folder import parsing_the_folder
 from delete_empty import delete_empty_folder
 def main():
     p = "C:\\Users\\verbo\\Documents\\Python\\Go IT\\6 home work\\Мусор"
-    make_new_dir(p)
-    parsing_the_folder(p)
-    delete_empty_folder(p)
+    make_new_dir(p) #Создаем папку
+    parsing_the_folder(p) #Итерируем папки
+    delete_empty_folder(p) #Удаляем пустые папки
 
 if __name__ == "__main__":
     main()
