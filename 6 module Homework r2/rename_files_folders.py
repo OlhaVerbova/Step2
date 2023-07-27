1f69702c1beece3a8dbfb05cb4a98f41e4f691fb
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2023-07-27 14:27:26 +0300

    Update rename_files_folders.py

--- 6 module Homework r2/rename_files_folders.py
+++ 6 module Homework r2/rename_files_folders.py
@@ -1,3 +1,43 @@
 from pathlib import Path
+from normolize import normalize
 def rename_all_folders_files(path_to_folder: str):
-    pass
+    path = Path(path_to_folder)
+    for item in path.iterdir():
+        if item.is_file(): 
+            rename = normalize(item.stem) + item.suffix
+            result_name = item.with_name(rename)
+            item.rename(result_name)
+        
+        elif item.is_dir():
+            rename = normalize(item.name)
+            result_name = item.with_name(rename)
+            item.rename(result_name) 
+
+            rename_all_folders_files(item)
+
+def rename_all_files(path_to_folder: str):
+    path = Path(path_to_folder)
+    for item in path.iterdir():
+        if item.is_file(): 
+            rename = normalize(item.stem) + item.suffix
+            result_name = item.with_name(rename)
+            item.rename(result_name)
+        
+        elif item.is_dir():           
+
+            rename_all_folders_files(item)
+
+def rename_all_folders(path_to_folder: str):
+    path = Path(path_to_folder)
+    for item in path.iterdir():
+        if item.is_file():
+             continue
+        
+        elif item.is_dir():
+            rename = normalize(item.name)
+            result_name = item.with_name(rename)
+            item.rename(result_name) 
+
+            rename_all_folders_files(item)
+
+    
